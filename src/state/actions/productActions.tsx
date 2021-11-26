6726c56f8f5632358d856117d83d97790cef3d8c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-26 15:16:11 +0100

    productList interface with productState to represent the initialState

--- src/state/actions/productActions.tsx
+++ src/state/actions/productActions.tsx
@@ -1,14 +1,28 @@
 import { ActionType } from '../action-types/index';
 // create a action type for every action we can omit with interfaces
 
+// initialState for product
+export interface ProductList {
+  id: number; // for mapping
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export interface productsState {
+  products: ProductList[];
+}
+
 interface setProductsAction {
   type: ActionType.SET_PRODUCTS;
-  payload: {}; // products
+  payload: ProductList[]; // products
 }
 
 interface selectedProductsAction {
   type: ActionType.SELECTED_PRODUCT;
-  payload: {};
+  payload: ProductList[];
 }
 
 interface removeProductAction {
