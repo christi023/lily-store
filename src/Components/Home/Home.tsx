281539d34cb7d1ab64d4df1661cf86940ccc9afb
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-26 15:25:48 +0100

    Home displays components

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// Components
+import Banner from '../Banner/Banner';
+import Carrousel from '../Carrousel/Carrousel';
+import Products from '../Products/Products';
+
+const Home = () => {
+  return (
+    <>
+      <div style={{ position: 'relative' }}>
+        <Carrousel slide1={'hero1'} slide2={'hero2'} slide3={'hero3'}>
+          <Banner title="Great Deals" subtitle="40% discounts starting now" />
+        </Carrousel>
+      </div>
+      <div>
+        <Products />
+      </div>
+    </>
+  );
+};
+
+export default Home;
