c71d929008c81c3a533ac5b26a0d7a1b65208608
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-26 15:16:56 +0100

    action creators

--- src/state/action-creators/index.tsx
+++ src/state/action-creators/index.tsx
@@ -1,18 +1,18 @@
 import { ActionType } from '../action-types';
-import { Action } from '../actions/productActions';
+import { Action, ProductList } from '../actions/productActions';
 import { Dispatch } from 'redux';
 
 // action-creators are functions that dispatch actions
-export const setProducts = (products: {}) => {
+export const setProducts = (product: ProductList[]) => {
   return (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SET_PRODUCTS,
-      payload: products,
+      payload: product,
     });
   };
 };
 
-export const selectedProducts = (product: {}) => {
+export const selectedProducts = (product: ProductList[]) => {
   return (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SELECTED_PRODUCT,
