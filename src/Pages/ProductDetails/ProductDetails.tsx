c10c8202c8684c38ba4f7afdd4fdaae8411fcf1b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-12-01 12:35:26 +0100

    product details for each product

--- src/Pages/ProductDetails/ProductDetails.tsx
+++ src/Pages/ProductDetails/ProductDetails.tsx
@@ -5,12 +5,25 @@ import { useDispatch, useSelector } from 'react-redux';
 // action
 import { selectedProducts } from '../../state/action-creators';
 import { State } from '../../state/reducers';
+import { Card, Col, Row } from 'react-bootstrap';
+//import { Link } from 'react-router-dom';
+import './ProductDetails.css';
 
 type ParamTypes = {
   productId: string;
 };
 
-const ProductDetails = () => {
+// props
+type ProductDetailsProps = {
+  image: string; // from api
+
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+};
+const ProductDetails = ({ title, price, image, description, category }: ProductDetailsProps) => {
   // access the selected product
   const product = useSelector((state: State) => state.product);
 
@@ -34,8 +47,41 @@ const ProductDetails = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Product details</h1>
+    <div className="">
+      <Row className="dd-flex justify-content-center">
+        <Col md={8}>
+          <Card className="card">
+            <Row>
+              <Col md={6}>
+                <div className="d-flex flex-row align-items-center">
+                  <i className="fa fa-apple fs-1"></i>
+                  <span className="fw-bold ms-1 fs-5">{title}</span>
+                </div>
+                <h1 className="fs-1 ms-1 mt-3">{category}</h1>
+                <div className="ms-1">
+                  <span>{description}</span>
+                </div>
+                <div className="mt-5 radio-buttons">
+                  <label className="radio">
+                    <input type="radio" name="code" value="grey" checked /> <span></span>
+                  </label>
+                  <label className="radio">
+                    <input type="radio" name="code" value="pink" /> <span></span>
+                  </label>
+                  <label className="radio">
+                    <input type="radio" name="code" value="black" /> <span></span>
+                  </label>
+                </div>
+                <div>
+                  <button className="button">
+                    <span>Order Now</span> <i className="ms-2 fa fa-long-arrow-right"></i>
+                  </button>
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
