0b3199c444fc9268e7eb43c4a1b16fee2d251036
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-12-04 14:02:43 +0100

    Holds details for each product card

--- src/Pages/Product/ProductCard.tsx
+++ src/Pages/Product/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Product.css';
 // props
@@ -15,44 +15,40 @@ type ProductCardProps = {
 
 const ProductCard = ({ id, title, price, image, description, category }: ProductCardProps) => {
   return (
-    <Col md={5}>
-      <Card className="">
-        <div className="view zoom overlay">
-          <h4 className="mb-0">
-            <span className="badge badge-primary badge-pill badge-news">Sale</span>
-          </h4>
-
-          <div className="mask">
+    <Col lg={3} md={4} sm={6} className="pb-1">
+      <div>
+        <div className="product-item bg-light mb-4">
+          <div className="product-img position-relative overflow-hidden">
             <Card.Img variant="top" className="img-fluid w-100" src={image} alt="" />
-            <div className="mask rgba-black-slight"></div>
+            <div className="product-action">
+              <Link to="/cart" className="btn btn-outline-dark btn-square" title="Add to cart">
+                <i className="fas fa-shopping-cart"></i>
+              </Link>
+              <Link
+                to="/wishlist"
+                className="btn btn-outline-dark btn-square"
+                title="Add to wishlist"
+              >
+                <i className="far fa-heart"></i>
+              </Link>
+              <Link
+                to={`/product/${id}`}
+                className="btn btn-outline-dark btn-square"
+                title="Details"
+              >
+                <i className="fas fa-info-circle"></i>
+              </Link>
+            </div>
           </div>
+          <Card.Body className="text-center py-4">
+            <Card.Title className=" text-decoration-none text-truncate">{title}</Card.Title>
+            <div className="d-flex align-items-center justify-content-center mt-2">
+              <h5>$ {price}</h5>
+            </div>
+          </Card.Body>
         </div>
-
-        <Card.Body className="text-center">
-          <div className="text-center">
-            <Card.Title className="fw-bolder">{title}</Card.Title>$ {price}
-          </div>
-          <Card.Text className="small text-muted text-uppercase mb-2">{description}</Card.Text>
-
-          <Link to="/cart" className="btn btn-primary btn-sm mr-1 mb-2">
-            <i className="fas fa-shopping-cart"></i> Add to cart
-          </Link>
-          <Link to={`/product/${id}`} className="btn btn-light btn-sm mr-1 mb-2">
-            <i className="fas fa-info-circle pr-2"></i>Details
-          </Link>
-          <Link
-            to="/wishlist"
-            className="btn btn-danger btn-sm px-3 mb-2 material-tooltip-main"
-            data-toggle="tooltip"
-            data-placement="top"
-            title="Add to wishlist"
-          >
-            <i className="far fa-heart"></i>
-          </Link>
-        </Card.Body>
-      </Card>
+      </div>
     </Col>
   );
 };
-
 export default ProductCard;
