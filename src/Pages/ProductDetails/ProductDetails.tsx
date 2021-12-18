cc4f12c5aeb07c5f8a06a9038841b5693f597bd3
Your Name <sheliannohlsson@yahoo.com>
2021-12-18 15:07:09 +0100

    product detail page for a product

--- src/Pages/ProductDetails/ProductDetails.tsx
+++ src/Pages/ProductDetails/ProductDetails.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 // action
-import { selectedProducts } from '../../state/action-creators';
-import { State } from '../../state/reducers';
-import { Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './ProductDetails.css';
+import { selectedProducts } from "../../state/action-creators";
+import { State } from "../../state/reducers";
+import { Card, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./ProductDetails.css";
 
 type ParamTypes = {
   productId: string;
@@ -17,12 +17,12 @@ const ProductDetails = () => {
   // access the selected product
   const product = useSelector((state: State) => state.product);
 
-  let { image, title, price, category, description }: any = product;
+  let { image, title, price, category, description, rating }: any = product;
 
   const { productId } = useParams<ParamTypes>();
   const dispatch = useDispatch();
 
-  console.log('hhhhhh', product);
+  console.log("hhhhhh", product);
 
   const fetchProductDetails = async () => {
     await axios
@@ -31,97 +31,62 @@ const ProductDetails = () => {
         const productDetails = response.data;
         dispatch(selectedProducts(productDetails));
       })
-      .catch((error) => console.log('Err', error));
+      .catch((error) => console.log("Err", error));
   };
 
   useEffect(() => {
-    if (productId && productId !== '') fetchProductDetails();
+    if (productId && productId !== "") fetchProductDetails();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="container">
-      <Col lg={8} className="border p-3 main-section md-white">
-        <Row className="m-0">
-          <Col lg={4} className="left-side-product-box pb-3">
-            <img className="fluid" src={image} alt="" />
-          </Col>
-          <Col md={6} lg={8}>
-            <div className="right-side-pro-detail border p-3 m-0">
-              <Row>
-                <Col lg={12}>
-                  <p className="m-0 p-0 mt-4">{title}</p>
-                </Col>
-                <Col lg={12}>
-                  <p className="m-0 p-0 price-pro">{price}</p>
-                </Col>
-                <Col lg={12} className=" pt-2">
-                  <h3>Product Detail</h3>
-                  <span>{description}</span>
-                </Col>
-                <Col lg={12}>
-                  <p className="tag-section">
-                    <strong>Tag : </strong>
-                    <Link to="/">{category}</Link>
-                  </p>
-                </Col>
-                <Col lg={12} className="mt-3">
-                  <Row>
-                    <Col lg={6} className="pb-4">
-                      <Link to="/cart" className="btn btn-1 ">
-                        <i className="fas fa-shopping-cart"> Add To Cart</i>
-                      </Link>
-                      <Link to="/" className="btn btn-1 ">
-                        <i className="fas fa-arrow-left"> Back</i>
-                      </Link>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </Col>
+      <Card>
+        <div className="container-fluid">
+          <Row className="wrapper">
+            <Col md={6} className="preview">
+              <div className="preview-pic tab-content">
+                <div className="tab-pane active" id="pic-1">
+                  <img className="" src={image} alt="" />
+                </div>
+              </div>
+            </Col>
+
+            <Col md={6} className="details">
+              <h3 className="product-title">{title}</h3>
+              <div className="rating">
+                <div className="stars">
+
+                  <span className="fa fa-star checked"></span>
+                  <span className="fa fa-star checked"></span>
+                  <span className="fa fa-star checked"></span>
+                  <span className="fa fa-star"></span>
+                  <span className="fa fa-star"></span>
+                </div>
+                <span className="review-no">{rating.count} reviews</span>
+              </div>
+
+              <p className="product-description">{description} </p>
+              <h5 className="price">
+                current price: <span>$ {price} </span>
+              </h5>
+              <h5 className="category">category : {category}</h5>
+
+              <div className="action">
+                <Link to="/cart" className="btn btn-1">
+                  <i className="fas fa-shopping-cart"> Add To Cart</i>
+                </Link>
+                <Link to="/" className="btn btn-1">
+                  <i className="fas fa-arrow-left"> Back</i>
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Card>
     </div>
   );
 };
 
-/* 
-    <div className="">
-      <Row className="dd-flex justify-content-center">
-        <Col md={8}>
-          <Card className="card">
-            <Row>
-              <Col md={6}>
-                <div className="d-flex flex-row align-items-center">
-                  <i className="fa fa-apple fs-1"></i>
-                  <span className="fw-bold ms-1 fs-5">{title}</span>
-                </div>
-                <h1 className="fs-1 ms-1 mt-3">{category}</h1>
-                <div className="ms-1">
-                  <span>{description}</span>
-                </div>
-                <div className="mt-5 radio-buttons">
-                  <label className="radio">
-                    <input type="radio" name="code" value="grey" checked /> <span></span>
-                  </label>
-                  <label className="radio">
-                    <input type="radio" name="code" value="pink" /> <span></span>
-                  </label>
-                  <label className="radio">
-                    <input type="radio" name="code" value="black" /> <span></span>
-                  </label>
-                </div>
-                <div>
-                  <button className="button">
-                    <span>Order Now</span> <i className="ms-2 fa fa-long-arrow-right"></i>
-                  </button>
-                </div>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-      </Row>
-</div>*/
 
 export default ProductDetails;
