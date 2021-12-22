dceb269681f24b4cf33130aa13918b1450d3a99e
Your Name <sheliannohlsson@yahoo.com>
2021-12-22 15:14:33 +0100

    removed initial state

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -3,6 +3,6 @@ import reducers from './reducers/index';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducers,  composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
