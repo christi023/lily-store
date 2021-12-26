0708e7d4e44e17adada33734a0b3cd60f1c4cbb2
Your Name <sheliannohlsson@yahoo.com>
2021-12-26 14:58:33 +0100

    imported fetchProducts action creator and clean up code

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,28 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import axios from 'axios';
-import { setProducts } from '../../state/action-creators/index';
+import {fetchProducts} from '../../state/action-creators/index';
 import Product from '../../Pages/Product/Product';
 
 const Products = () => {
   const dispatch = useDispatch();
-  /*const products1 = useSelector((state: State) => state.allProducts);
-  console.log(products1);*/
-
-  const fetchProducts = async () => {
-    await axios
-      .get('https://fakestoreapi.com/products')
-      .then((response: any) => {
-        const products = response.data;
-        console.log(products);
-        dispatch(setProducts(products));
-      })
-      .catch((err) => console.log('Err', err));
-  };
-
+  
   useEffect(() => {
-    fetchProducts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    dispatch(fetchProducts());
+     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
