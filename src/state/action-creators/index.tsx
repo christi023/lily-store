b02479bd360f56e3acca11cafbdc83e418e3cc63
Your Name <sheliannohlsson@yahoo.com>
2021-12-26 14:55:53 +0100

    thunk middleware for Fetch Products

--- src/state/action-creators/index.tsx
+++ src/state/action-creators/index.tsx
@@ -1,8 +1,22 @@
 import { ActionType } from '../action-types';
 import { Action, ProductList } from '../actions/productActions';
 import { Dispatch } from 'redux';
+import fakeStoreApi from '../../apis/fakeStoreApi';
 
 // action-creators are functions that dispatch actions
+// middleware thunk helps to make async call inside action creator
+export const fetchProducts = () => async (dispatch: Dispatch<Action>) =>{
+ const response = await fakeStoreApi.get("/products")
+  dispatch({type:ActionType.FETCH_PRODUCTS, payload:response.data})
+  } 
+
+  // get a product detail
+export const fetchProduct = (id: string) => async (dispatch: Dispatch<Action>) =>{
+ const response = await fakeStoreApi.get(`/products/${id}`)
+  console.log('rrr', response)
+  dispatch({type:ActionType.SELECTED_PRODUCT, payload:response.data})
+  } 
+
 export const setProducts = (products: ProductList[]) => {
   return (dispatch: Dispatch<Action>) => {
     dispatch({
